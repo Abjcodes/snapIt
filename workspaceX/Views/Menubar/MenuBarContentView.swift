dcf9d381a54da4564bdcbd82b428c0a7c94ad138
Abijith Vasanthakumar <ij8l@allegiantair.com>
2025-01-14 22:30:21 +0530

    Added menubarapp

--- workspaceX/Views/Menubar/MenuBarContentView.swift
+++ workspaceX/Views/Menubar/MenuBarContentView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct MenuBarContentView: View {
+    @StateObject private var viewModel = WorkspaceViewModel()
+    
     var body: some View {
-        Text("SnapIt")
+        Group {
+            if viewModel.workspaces.isEmpty {
+                Text("No workspaces found")
+                    .foregroundColor(.secondary)
+                    .padding()
+            } else {
+                ForEach(viewModel.workspaces) { workspace in
+                    WorkspaceMenuItem(workspace: workspace)
+                }
+                
+                Divider()
+            }
+            
+            Button("Open Workspace X") {
+                NSApp.activate(ignoringOtherApps: true)
+            }
+        }
+        .padding(.vertical, 4)
+    }
+}
+
+struct WorkspaceMenuItem: View {
+    let workspace: Workspace
+    
+    var body: some View {
+        Button {
+            launchWorkspace(workspace)
+        } label: {
+            HStack {
+                Image(systemName: "folder.fill")
+                    .foregroundColor(.accentColor)
+                Text(workspace.name)            }
+            .contentShape(Rectangle())
+        }
+    }
+    
+    private func launchWorkspace(_ workspace: Workspace) {
+        for item in workspace.items {
+            let url = URL(fileURLWithPath: item.path)
+            NSWorkspace.shared.open(url)
+            
+            if item.layout != .default {
+                WindowManager.shared.positionWindow(forItem: item)
+            }
+        }
     }
 }
 
--- workspaceX/workspaceXApp.swift
+++ workspaceX/workspaceXApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct workspaceXApp: App {
+    @StateObject private var workspaceViewModel = WorkspaceViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(workspaceViewModel)
         }
         MenuBarExtra("SnapIt", systemImage: "uiwindow.split.2x1") {
             MenuBarContentView()
+                .environmentObject(workspaceViewModel)
         }
     }
 }
